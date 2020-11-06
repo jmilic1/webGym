ccb06dc45c9bbc5360582e6b9cbd9c8f91bcadad
eduard.duras <eduard.duras@gmail.com>
2020-11-06 20:30:50 +0100

    added registration and  logout, backendURL is now stored in App components state

--- IzvorniKod/src/App.js
+++ IzvorniKod/src/App.js
@@ -6,19 +6,43 @@ import Homepage from './pages/homepage/homepage.component'
 import AuthPage from './pages/authPage/authPage.component'
 import GymList from './pages/gymList/gymList.component'
 
-function App() {
-  return (
-    <div className="App">
-        <Header />
-        <div className = 'page-container'>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/auth" component={AuthPage} />
-            <Route exact path="/gymList" component={GymList} />
-          </Switch>
-        </div>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      name: "",
+      surname: "",
+      coach: false,
+      admin: false,
+      gymOwner: false
+    }
+  }
+
+  handleLogin = (name, surname, coach, admin, gymOwner) => {
+    this.setState({
+      name:name,
+      surname: surname,
+      coach: coach,
+      admin:admin,
+      gymOwner: gymOwner
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Header />
+          <div className = 'page-container'>
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/auth" render={(props) => <AuthPage {...props} handleLogin={this.handleLogin} />} />
+              <Route exact path="/gymList" component={GymList} />
+            </Switch>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
