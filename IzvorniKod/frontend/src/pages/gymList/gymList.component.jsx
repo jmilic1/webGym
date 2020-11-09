e020e7ca3f8330a6bd215b4620138120fbe34d8a
Fran Sodić <fran.sodic@fer.hr>
2020-11-09 16:52:08 +0100

    updated gitignore; added frontend directory to IzvorniKod

---
+++ IzvorniKod/frontend/src/pages/gymList/gymList.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './gymList.styles.scss'
+import GymMetaDataContainer from '../../components/gym-metadata-container/gym-metadata-container.component'
+
+class GymList extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            gymList: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://web-gym2.herokuapp.com/gymList', {
+            method: 'GET',
+            credentials: "include"
+        }).then(response => {
+            return response.json()
+        }).then(gymList => {
+            this.setState({
+                gymList: gymList
+            })
+        })
+    }
+
+    render(){
+        return(
+
+            <div className = 'gymList-container'>
+                <div className = 'gymList-header-container'>
+                    <h3>Ime teretane</h3>
+                    <h3>Grad</h3>
+                    <h3>Informacije</h3>
+                </div>
+                {this.state.gymList.map(gym =>
+                    <GymMetaDataContainer name = {gym.name} city = {gym.city} key = {gym.id}/>
+                )}
+            </div>
+        )
+    }
+}
+
+export default GymList
