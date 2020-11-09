985da7720f508ef7b13d721f30881d0da535b9a4
Jura Milić <jura.milic@fer.hr>
2020-11-09 18:06:32 +0000

    Update gym-metadata-container.component.jsx

--- IzvorniKod/frontend/src/components/gym-metadata-container/gym-metadata-container.component.jsx
+++ IzvorniKod/frontend/src/components/gym-metadata-container/gym-metadata-container.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './gym-metadata-container.styles.scss'
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import {Link} from 'react-router-dom'
 
-const GymMetaDataContainer = ({name}) => (
+const GymMetaDataContainer = ({name, city}) => (
     <div className = 'gym-metadata-container'>
-        <p>{name}</p>
-        <p>Prostor za ostale podatke</p>
+        <p className = 'gym-grid-content'>{name}</p>
+        <p className = 'gym-grid-content'>{city}</p>
+        <Link className = 'gym-grid-content' to = '/gymList'><AiOutlineInfoCircle /></Link>
     </div>
 )
 
