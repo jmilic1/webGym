fe28646681a2536b84df2d1f61b88df3b620f6c4
Jura Milić <jura.milic@fer.hr>
2020-11-09 16:52:48 +0000

    Update header.component.jsx

--- IzvorniKod/frontend/src/components/header/header.component.jsx
+++ IzvorniKod/frontend/src/components/header/header.component.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import './header.styles.scss'
 import {Link} from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 
-const Header = () => (
-    <div className = 'header-container'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo'/>
-        </Link>
-        <div className = 'link-container'>
-            <Link to = '/gymList'>Popis teretana</Link>
-            {/*<Link to='/'>Moj racun</Link>*/}
-            <Link to = '/auth'>Prijava</Link>
-        </div>
-    </div>
-)
+class Header extends React.Component {
+
+    render(){
+        return(
+            <div className = 'header-container'>
+                <Link className='logo-container' to='/'>
+                    <Logo className='logo'/>
+                </Link>
+                <div className = 'link-container'>
+                    <Link to = '/gymList'>Popis teretana</Link>
+
+                    {this.props.loggedIn ?
+                        <Link to = '/logOut'>Odjava</Link>
+                        :
+                        <Link to = '/auth'>Prijava</Link>
+
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
 
 export default Header
