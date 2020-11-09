3e886b5a66dfaea57c30cec5f056853c74bef73a
Milic <Jura.Milic@avl.com>
2020-11-09 18:49:35 +0100

    Fixed app.js and header.component.jsx imports

--- IzvorniKod/frontend/src/App.js
+++ IzvorniKod/frontend/src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import './App.css';
+import { Switch, Route} from 'react-router-dom';
+import Header from './components/header/header.component'
 import Homepage from './pages/homepage/homepage.component'
 import AuthPage from './pages/authPage/authPage.component'
 import GymList from './pages/gymList/gymList.component'
--- IzvorniKod/frontend/src/components/header/header.component.jsx
+++ IzvorniKod/frontend/src/components/header/header.component.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import './header.styles.scss'
 import {Link} from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 
