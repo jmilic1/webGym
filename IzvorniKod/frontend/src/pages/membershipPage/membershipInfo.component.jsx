3e181295bc19852c46ff90652216d0c440095007
Fran Sodić <fran.sodic@fer.hr>
2021-01-08 11:13:34 +0100

    implemented membership info page

--- IzvorniKod/frontend/src/pages/membershipPage/membershipInfo.component.jsx
+++ IzvorniKod/frontend/src/pages/membershipPage/membershipInfo.component.jsx
@@ -5,7 +5,8 @@ class MembershipInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            membership: null
+            membership: null,
+            gymName: ''
         }
     }
 
@@ -35,12 +36,63 @@ class MembershipInfo extends React.Component {
             alert("Došlo je do pogreške: " + e.message)
         })
         console.log(this.state.membership)
+        // await this.fetchGymName()
     }
+
+    // async fetchGymName() {
+    //     const url = this.props.backendURL + "gymInfo?";
+    //
+    //
+    //     const params = {id: this.state.membership?.idGym};
+    //     const searchParams = new URLSearchParams(params);
+    //
+    //     await fetch(url + searchParams.toString(), {
+    //         method: "GET",
+    //         headers: { 'Content-Type': 'application/json' },
+    //         credentials: 'include'
+    //     }).then(res => {
+    //         if (!res.ok) {
+    //             throw new Error("HTTP Error! " + res.status)
+    //         } else {
+    //             return res.json()
+    //         }
+    //     }).then(gym => {
+    //             this.setState({
+    //                 gym: gym
+    //             })
+    //         }
+    //     ).catch(e => {
+    //         alert("Došlo je do pogreške: " + e.message)
+    //     })
+    //     console.log(this.state.gym)
+    // }
 
 
     render() {
         return (
-           <div></div>
+           <div className='membership-page-container'>
+               <div className='membership-container'>
+                   {this.state.membership &&
+                   <div className='user-info-form'>
+                       <div className='userInfo-formInput'>
+                           <label htmlFor='interval'>Trajanje</label>
+                           <input type='text' value={this.state.membership.interval} name='interval'
+                                  disabled={!this.state.modifyGymInfo}/>
+                       </div>
+                       <div className='userInfo-formInput'>
+                           <label htmlFor='description'>Opis</label>
+                           <input type='text' value={this.state.membership.description} name='description'
+                                  disabled={!this.state.modifyGymInfo}/>
+                       </div>
+                       <div className='userInfo-formInput'>
+                           <label htmlFor='price'>Cijena</label>
+                           <input type='text' value={this.state.membership.price} name='price'
+                                  disabled={!this.state.modifyGymInfo}/>
+                       </div>
+                   </div>
+                   }
+               </div>
+           </div>
         );
     }
 }
