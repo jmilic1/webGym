e04bb1b1e90c1ba3f8f2e84bf245f3325ccce8bb
jMilic <Jura.Milic@avl.com>
2021-01-11 01:18:27 +0100

    Remove unnecessary imports.

--- IzvorniKod/backend/src/main/java/hr/fer/progi/bugbusters/webgym/service/CoachService.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/bugbusters/webgym/service/CoachService.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service("coachService")
 public class CoachService {
