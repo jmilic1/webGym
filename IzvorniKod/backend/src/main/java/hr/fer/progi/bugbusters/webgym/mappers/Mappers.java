b8588aaf316496b551976165dac3a34947809ca7
jMilic <Jura.Milic@avl.com>
2021-01-11 01:11:51 +0100

    Added more mapping methods in Mappers class.
    Mappings for: User, Goal, Gym, GymLocation, Plan all of which have their dto->entity and entity->dto methods.

--- IzvorniKod/backend/src/main/java/hr/fer/progi/bugbusters/webgym/mappers/Mappers.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/bugbusters/webgym/mappers/Mappers.java
@@ -1,25 +1,53 @@
 package hr.fer.progi.bugbusters.webgym.mappers;
 
 import hr.fer.progi.bugbusters.webgym.model.*;
-import hr.fer.progi.bugbusters.webgym.model.dto.CoachDto;
-import hr.fer.progi.bugbusters.webgym.model.dto.JobRequestDto;
-import hr.fer.progi.bugbusters.webgym.model.dto.PlanClientDto;
-import hr.fer.progi.bugbusters.webgym.model.dto.PlanDto;
+import hr.fer.progi.bugbusters.webgym.model.dto.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class Mappers {
+    private static ModelMapper modelMapper;
 
-    public static PlanDto mapPlanToPlanDto(Plan plan) {
-        PlanDto planDto = new PlanDto();
+    @Autowired
+    public Mappers(ModelMapper modelMapper) {
+        Mappers.modelMapper = modelMapper;
+    }
 
-        planDto.setId(plan.getId());
-        planDto.setCoachUsername(plan.getUser().getUsername());
-        planDto.setDescription(plan.getDescription());
-        planDto.setDateFrom(plan.getDateFrom().toString());
-        planDto.setDateTo(plan.getDateTo().toString());
-        planDto.setPrice(plan.getPrice());
-        planDto.setIsTraining(plan.getIsTraining());
+    public static User mapDtoToUser(UserDto userDto) { return modelMapper.map(userDto, User.class); }
+    public static UserDto mapUserToDto(User user) {
+        return modelMapper.map(user, UserDto.class);
+    }
 
-        return planDto;
+    public static Goal mapDtoToGoal(GoalDto goalDto) { return modelMapper.map(goalDto, Goal.class); }
+    public static GoalDto mapGoalToDto(Goal goal) { return modelMapper.map(goal, GoalDto.class); }
+
+    public static Gym mapDtoToGym(GymDto gymDto) { return modelMapper.map(gymDto, Gym.class); }
+    public static GymDto mapGymToDto(Gym gym) { return modelMapper.map(gym, GymDto.class); }
+
+    public static GymLocation mapDtoToLocation(GymLocationDto gymLocationDto){
+        return modelMapper.map(gymLocationDto, GymLocation.class);
+    }
+    public static GymLocationDto mapLocationToDto(GymLocation gymLocation){
+        GymLocationDto gymLocationDto = modelMapper.map(gymLocation, GymLocationDto.class);
+        gymLocationDto.setId(gymLocation.getGym().getId());
+        return gymLocationDto;
+    }
+
+    public static Plan mapDtoToPlan(PlanDto planDto, User user){
+        Plan plan = modelMapper.map(planDto, Plan.class);
+        plan.setUser(user);
+        return plan;
+    }
+    public static PlanDto mapPlanToDto(Plan plan) {
+        PlanDto dto = modelMapper.map(plan, PlanDto.class);
+        dto.setCoachUsername(plan.getUser().getUsername());
+        return dto;
     }
 
     public static JobRequest mapDtoToJobRequest(JobRequestDto jobRequestDto, User user, Gym gym) {
@@ -28,7 +56,6 @@ public class Mappers {
         jobRequest.setState(JobRequestState.IN_REVIEW);
         jobRequest.setUser(user);
         jobRequest.setGym(gym);
-
         return jobRequest;
     }
 
