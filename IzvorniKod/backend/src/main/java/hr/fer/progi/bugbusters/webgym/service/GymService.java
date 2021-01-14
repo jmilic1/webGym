9093ec66e8703e7dc4b44a1c709a0604b3432ea3
mercepluka <luka.mercep@axilis.com>
2021-01-14 23:31:40 +0100

    fix 2

--- IzvorniKod/backend/src/main/java/hr/fer/progi/bugbusters/webgym/service/GymService.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/bugbusters/webgym/service/GymService.java
@@ -357,7 +357,7 @@ public class GymService {
         for (GymUser gymUser : gym.getGymUsers()) {
             if (gymUser.getUser().getUsername().equals(username)) ownsGym = true;
         }
-        if (ownsGym) throw new IllegalArgumentException("405");
+        if (!ownsGym) throw new IllegalArgumentException("405");
 
         List<GymUser> gymUserList = gymUserRepository.findAll();
         for (GymUser gymUser : gymUserList) {
