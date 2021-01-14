ba96396eb159aebdb8b7b63a0d3662b8b7f6fd1b
Fran Sodic <fran.sodic@gmail.com>
2021-01-14 22:16:04 +0100

    fixed date bug

--- IzvorniKod/frontend/src/components/adminTransaction/adminTransaction.component.jsx
+++ IzvorniKod/frontend/src/components/adminTransaction/adminTransaction.component.jsx
@@ -5,7 +5,7 @@ const AdminTransaction = ({ date, sender, receiver, amount, type }) => {
     const datum = new Date(date)
     return (
         <div className='user-metadata-container'>
-            <p className='user-grid-content'>{datum.getDay()}.{datum.getMonth() + 1}.{datum.getFullYear()}</p>
+            <p className='user-grid-content'>{datum.getDate()}.{datum.getMonth() + 1}.{datum.getFullYear()}</p>
             <p className='user-grid-content'>{sender}</p>
             <p className='user-grid-content'>{receiver}</p>
             <p className='user-grid-content'>{amount}</p>
--- IzvorniKod/frontend/src/components/transaction/transaction.component.jsx
+++ IzvorniKod/frontend/src/components/transaction/transaction.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './transaction.styles.css'
 
-const Transaction = ({amount, dateWhen, senderUsername, receiverUsername, transactionType}) => {
+const Transaction = ({ amount, dateWhen, senderUsername, receiverUsername, transactionType }) => {
     const datum = new Date(dateWhen)
     return (
         <div className='transaction-container'>
-                <p>Iznos: {amount}</p>
-                <p>Datum: {datum.getDay()}.{datum.getMonth() + 1}.{datum.getFullYear()}.</p>
-                <p>Pošiljatelj: {senderUsername}</p>
-                <p>Primatelj: {receiverUsername}</p>
-                <p>Tip transakcije: {transactionType}</p>
+            <p>Iznos: {amount}</p>
+            <p>Datum: {datum.getDate()}.{datum.getMonth() + 1}.{datum.getFullYear()}.</p>
+            <p>Pošiljatelj: {senderUsername}</p>
+            <p>Primatelj: {receiverUsername}</p>
+            <p>Tip transakcije: {transactionType}</p>
         </div>
     )
 }
